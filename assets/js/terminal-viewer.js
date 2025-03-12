44514cb0fe49ba7e0f173ac6e01532f1f65f29d0
SilverFS <45943209+silverfs@users.noreply.github.com>
2025-03-12 01:16:40 +0100

    Remove unused animation conditions

--- assets/js/terminal-viewer.js
+++ assets/js/terminal-viewer.js
@@ -11,13 +11,8 @@ $.fn.typewriter = function(options) {
     c.html("");
     var e = function() {
       if ("<" == b.substring(a, a + 1)) {
-        var f = new RegExp(/<span class="instant"/),
-          g = new RegExp(/<span class="clear"/);
-        if (b.substring(a, b.length).match(f)) a += b.substring(a, b.length).indexOf("</span>") + 7;
-        else if (b.substring(a, b.length).match(g)) d = a, a += b.substring(a, b.length).indexOf("</span>") + 7;
-        else
-          for (;
-            ">" != b.substring(a, a + 1);) a++
+        // Skip over HTML tags
+        for (; ">" != b.substring(a, a + 1);) a++;
       }
       c.html(b.substring(d, a++) + '<span class="cursor">' + (a & 3 ? "_" : "") + '</span>');
       if (c.scrollTop() + c.innerHeight() >= c[0].scrollHeight - 40) { // Allow a small buffer
@@ -31,6 +26,7 @@ $.fn.typewriter = function(options) {
     };
     e();
   });
-  return this
+  return this;
 };
+
 $(".terminal").typewriter();
