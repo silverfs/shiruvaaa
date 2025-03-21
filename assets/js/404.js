b07d423821f555a88141918159204f4306b53ab7
SilverFS <45943209+silverfs@users.noreply.github.com>
2025-03-21 03:04:17 +0100

    Shortened 404 message appearance

--- assets/js/404.js
+++ assets/js/404.js
@@ -50,5 +50,5 @@ $(document).ready(function() {
     isInitialMessage = false;
     changeMessage();
     setInterval(changeMessage, 12000); // Change message every 12 seconds
-  }, 15000);
+  }, 14000);
 });
